7efc50e63ec5d1f59002aabb4030e1e162ed4e68
e-kif <yevhen>
2024-09-09 10:00:06 +0300

    index template and static css are implemented

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+blog_posts = [{}, {}]
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
