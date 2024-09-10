ff8ac64bd8a29bfccbbb2eb7686241be6c332859
e-kif <yevhen>
2024-09-10 23:14:37 +0300

    '/add' route is added for adding new post

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
-blog_posts = [{}, {}]
+blog_posts = []
 
 
 @app.route('/')
 def hello_world():
 
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_post():
+    if request.method == 'POST':
+        author = request.form.get('author')
+        title = request.form.get('title')
+        post = request.form.get('post')
+        if not blog_posts:
+            post_id = 1
+        else:
+            post_id = blog_posts[-1]['id'] + 1
+        post_dict = {'id': post_id,
+                     'author': author,
+                     'title': title,
+                     'content': post}
+        blog_posts.append(post_dict)
+
+        return redirect('/')
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
